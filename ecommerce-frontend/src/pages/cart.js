332ec4f7b39ceb3e594dc829311af288669765ad
Khyati Agarwal <khyatia43@gmail.com>
2025-06-26 10:43:02 +0530

    remove split when getting imageUrl in the cart drawer

--- ecommerce-frontend/src/pages/cart.js
+++ ecommerce-frontend/src/pages/cart.js
@@ -55,7 +55,7 @@ export default function CartDrawer() {
         <div className={styles.body}>
           {cart.map(item => (
             <div key={item.id} className={styles.item}>
-             <img src={item.imageUrl.split(',')[0]} alt={item.name} className={styles.image} />
+             <img src={item.imageUrl[0]} alt={item.name} className={styles.image} />
 
               <div className={styles.details}>
                 <h4>{item.name}</h4>

10bf784b74168cf7838b87dda64ff4475c3ab156
Khyati Agarwal <khyatia43@gmail.com>
2025-06-27 17:15:28 +0530

    handled single comma in FeaturedProducts component

--- ecommerce-frontend/src/components/FeaturedProducts.tsx
+++ ecommerce-frontend/src/components/FeaturedProducts.tsx
@@ -71,7 +71,7 @@ export default function FeaturedProducts() {
           Best Sellers
         </h2>
         <p className="text-gray-500 mb-12 text-lg">
-          Discover formulas our customers can't get enough of
+          Discover formulas our customers can&rsquo;t get enough of
         </p>
   
         <div className="grid gap-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">

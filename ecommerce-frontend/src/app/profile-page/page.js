2f419e17b2b7c1dd16b04ca7d5fea7166d2bdc19
Khyati Agarwal <khyatia43@gmail.com>
2025-06-30 16:40:49 +0530

    commented Link in profile-page

--- ecommerce-frontend/src/app/profile-page/page.js
+++ ecommerce-frontend/src/app/profile-page/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useUser } from '@/context/UserContext'
 import Image from 'next/image'
-import Link from 'next/link'
+// import Link from 'next/link'
 
 export default function ProfilePage() {
   const { user } = useUser()
